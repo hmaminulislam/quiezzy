bdec100583e5d7df4478b938fd1a74274fc7b1b0
Aminul <mdaminulislamsahin14@gmail.com>
2022-10-12 13:14:36 +0600

    rechart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React from "react";
+import './Statistics.css';
+import { useLoaderData } from "react-router-dom";
+import {
+  BarChart, 
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const Statistics = () => {
+  const quiz = useLoaderData();
+  const data = quiz.data;
   return (
-    <div>
-      <h2>Statistics</h2>
+    <div className="chart-container">
+      <h2 className="text-5xl font-bold text-sky-400 mt-10 mb-5">Quiz Chart</h2>
+      <BarChart width={400} height={400} data={data}>
+        <Bar dataKey="total" fill="#8884d8" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+      </BarChart>
     </div>
   );
 };
